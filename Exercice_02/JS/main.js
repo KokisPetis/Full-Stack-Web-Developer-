ac62f54301ceb8ebdc0650f674c6e3f76696a879
KokisPetis <kokefraile@hotmail.com>
2022-11-11 21:20:15 +0100

    I upload the first 4 Exercices

---
+++ Exercice_02/JS/main.js
@@ -0,0 +1,4 @@
+
+//documento JavaScript donde añadiremos los scripts que queramos.
+
+document.write("Hello World"); //Document.write sirve para mostrar por pantalla el elemnto que tengamos entre comillas dentro de los parentersis.
